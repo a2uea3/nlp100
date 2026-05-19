faf331d12c447ff97dc96cb531916f8b7839adca
a2uea3 <2420350@mwu.jp>
2026-05-19 17:12:42 +0900

    2026/5/19 nlp100/00

--- nlp01/00.py
+++ nlp01/00.py
@@ -1,1 +1,11 @@
-テスト１
+#00. パタトクカシーー,2つの文字列「パトカー」と「タクシー」の文字を先頭から交互に連結し、文字列「パタトクカシーー」を得よ。
+kotoba1= "パトカー"
+kotoba2 = "タクシー"
+
+kekka0 = ""
+
+for i in range(len(kotoba1)):
+    kekka0 += kotoba1[i] + kotoba2[i]
+print(kekka0)
+
+
